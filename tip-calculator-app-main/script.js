3e71cb3229df02e564af0ec1b5c2778333d45625
Afonso ComboVideos <afonso.bezerra@combovideos.com.br>
2021-12-30 18:56:09 -0300

    Subindo Calculadora

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,99 @@
+function tips(value) {
+
+    var input5 = document.getElementById('input5');
+    var input10 = document.getElementById('10');
+    var input15 = document.getElementById('15');
+    var input25 = document.getElementById('25');
+    var input50 = document.getElementById('50');
+    var inputcustom = document.getElementById('custom');
+
+    if (value == 5) {
+        input5.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        input5.style.color = 'hsl(183, 100%, 15%)';
+        input10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input10.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input15.style.color = 'white';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input50.style.color = 'white';
+        inputcustom.style.backgroundColor = 'hsl(180, 2%, 88%)';
+    }
+
+    if (value == 10) {
+        input5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input5.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input15.style.color = 'white';
+        input10.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        input10.style.color = 'hsl(183, 100%, 15%)';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input50.style.color = 'white';
+        inputcustom.style.backgroundColor = 'hsl(180, 2%, 88%)';
+    }
+
+    if (value == 15) {
+        input5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input5.style.color = 'white';
+        input10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input10.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        input15.style.color = 'hsl(183, 100%, 15%)';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input50.style.color = 'white';
+        inputcustom.style.backgroundColor = 'hsl(180, 2%, 88%)';
+    }
+
+    if (value == 25) {
+        input5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input5.style.color = 'white';
+        input10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input10.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input15.style.color = 'white';
+        input25.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        input25.style.color = 'hsl(183, 100%, 15%)';
+        input50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input50.style.color = 'white';
+        inputcustom.style.backgroundColor = 'hsl(180, 2%, 88%)';
+    }
+
+    if (value == 50) {
+        input5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input5.style.color = 'white';
+        input10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input10.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input15.style.color = 'white';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input50.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        input50.style.color = 'hsl(183, 100%, 15%)';
+        inputcustom.style.backgroundColor = 'hsl(180, 2%, 88%)';
+    }
+
+    if (value == 'Custom') {
+        input5.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input5.style.color = 'white';
+        input10.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input10.style.color = 'white';
+        input15.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input15.style.color = 'white';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input25.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input25.style.color = 'white';
+        input50.style.backgroundColor = 'hsl(183, 100%, 15%)';
+        input50.style.color = 'white';
+        inputcustom.style.backgroundColor = 'hsl(172, 67%, 45%)';
+        inputcustom.style.color = 'hsl(183, 100%, 15%)';
+    }
+
+
+
+}
